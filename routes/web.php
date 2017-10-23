40e11897b91b1b2e7480cb654210740160daa15b
yc <718653142@qq.com>
2017-10-23 17:39:52 +0800

    add password resets & email configuration

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,9 @@ Route::delete('logout', 'SessionController@destroy')->name('logout');
 
 //激活账户#
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+//重置密码
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
